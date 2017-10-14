da8712cdb6aa053324d4d51c773cd6e77098f666
Konrad Czarnota <k.czarnota27@gmail.com>
2017-10-14 17:43:40 +0200

    Stations list

--- app/src/main/java/com/example/konrad/metroperyskop/MapActivity.java
+++ app/src/main/java/com/example/konrad/metroperyskop/MapActivity.java
@@ -8,8 +8,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,12 +20,28 @@ import butterknife.ButterKnife;
 public class MapActivity extends AppCompatActivity implements View.OnTouchListener
 
 {
+    @BindView(R.id.stations_list) ListView stationList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
+
+        String[] stations = getResources().getStringArray(R.array.stations_array);
+        //Row layout defined by Android: android.R.layout.simple_list_item_1
+        stationList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.list_item, stations));
+        stationList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
