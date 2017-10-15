d88108fac5b243497e8de33a923ed18ec1f7014f
Konrad Czarnota <k.czarnota27@gmail.com>
2017-10-15 13:43:14 +0200

    Bigger scanning activity

--- app/src/main/java/com/example/konrad/metroperyskop/ScanActivity.java
+++ app/src/main/java/com/example/konrad/metroperyskop/ScanActivity.java
@@ -9,14 +9,6 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
